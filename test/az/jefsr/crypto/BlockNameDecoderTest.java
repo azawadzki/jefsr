6b4b77c425b3b9c7435740283558a563484947f1
Andrzej Zawadzki <azawadzki@gmail.com>
2012-01-10 21:04:56 +0100

    Basic tests for BlockNameDecoder

--- test/az/jefsr/crypto/BlockNameDecoderTest.java
+++ test/az/jefsr/crypto/BlockNameDecoderTest.java
@@ -1,19 +1,45 @@
 package az.jefsr.crypto;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import az.jefsr.config.Config;
+import az.jefsr.crypto.fixtures.FSFixture;
+import az.jefsr.crypto.fixtures.FSParanoid;
 
 public class BlockNameDecoderTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		fsFixture = new FSParanoid();
 	}
 
 	@Test
-	public void testDecodePath() {
-		fail("Not yet implemented");
+	public void testDecodeCorrectPath() throws CipherConfigException, CipherDataException {
+		Config config = fsFixture.getConfig();
+		Coder coder = Coder.Factory.getInstance().createInstance(config.getCipherAlg().getName(), fsFixture.getVolumeKey(), config);
+		NameDecoder nameDec = NameDecoder.Factory.getInstance().createInstance(config.getNameAlg().getName(), coder, config);
+		
+		String in1 = "7yp5rUju7WJz0HqxocaWPm9K";
+		String out1 = "readme.txt";
+		assertThat(nameDec.decodePath(in1), equalTo(out1));
+		
+		String in2 = "YKGAxrfWfbxeek7,t-L35lZN/TTUy3UG3HqBkN5cOTP,s0PLf/PBU6Fr0tBITS53aZLnmNQJL7";
+		String out2 = "1/2b/3.txt";
+		assertThat(nameDec.decodePath(in2), equalTo(out2));
 	}
-
+	
+	@Test(expected=CipherDataException.class)
+	public void testDecodeWrongPath() throws CipherDataException, CipherConfigException {
+		Config config = fsFixture.getConfig();
+		Coder coder = Coder.Factory.getInstance().createInstance(config.getCipherAlg().getName(), fsFixture.getVolumeKey(), config);
+		NameDecoder nameDec = NameDecoder.Factory.getInstance().createInstance(config.getNameAlg().getName(), coder, config);
+		
+		nameDec.decodePath("dummy");
+	}
+	
+	static FSFixture fsFixture;
 }
