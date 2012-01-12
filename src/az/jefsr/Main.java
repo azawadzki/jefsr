935ae522f75f2fd2f259ed78d172660ef0f5e092
Andrzej Zawadzki <azawadzki@gmail.com>
2012-01-12 20:20:22 +0100

    Test data for paranoid/blowfish combination

--- src/az/jefsr/Main.java
+++ src/az/jefsr/Main.java
@@ -17,19 +17,28 @@ public class Main {
 	 */
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedFormatException, CipherDataException, CipherConfigException {
 		//Security.addProvider(new BouncyCastleProvider());
-		String file = "test-data/paranoid_wout_name_ivchaining/.encfs6.xml";
+		//String file = "test-data/paranoid/.encfs6.xml";
+		//String file = "test-data/paranoid_wout_name_ivchaining/.encfs6.xml";
+		String file = "test-data/paranoid_with_blowfish/.encfs6.xml";
     	String userPassword = "test";
     	
     	Volume v = new Volume(file);
     	v.init(userPassword);
 
     	String[] files = {
-    			/*
-    			"YKGAxrfWfbxeek7,t-L35lZN/TTUy3UG3HqBkN5cOTP,s0PLf/PBU6Fr0tBITS53aZLnmNQJL7",
-    			"7yp5rUju7WJz0HqxocaWPm9K"
-    			*/
-    			"pqW447x1v6JOeeyyr-pCswIe",
-    			"/dJzawbcZtrPxVSJmvj,tf4rp////../dJzawbcZtrPxVSJmvj,tf4rp/./g9hJRg6V97eEtutYr8Zn-Eg5/yU,RRKp4frFbotJJt6tdl-oP"
+    			//paranoid
+    			//"YKGAxrfWfbxeek7,t-L35lZN/TTUy3UG3HqBkN5cOTP,s0PLf/PBU6Fr0tBITS53aZLnmNQJL7",
+    			//"7yp5rUju7WJz0HqxocaWPm9K"
+    			
+    			//paranoid_wout_name_ivchaining
+    			//"pqW447x1v6JOeeyyr-pCswIe",
+    			//"/dJzawbcZtrPxVSJmvj,tf4rp////../dJzawbcZtrPxVSJmvj,tf4rp/./g9hJRg6V97eEtutYr8Zn-Eg5/yU,RRKp4frFbotJJt6tdl-oP"
+    			
+    			//paranoid_with_blowfish*/
+    			"LNYp9njYE5P91oyK1U1bxEK6",
+    			"LztuRTXf9NA901/JFuxAg2CSSubN-/jvaSz-VrswGR0-"
+
+    			
     	};
     	for (String f: files) {
     		System.out.printf("%s\n%s\n--\n", f, v.decryptPath(f));
